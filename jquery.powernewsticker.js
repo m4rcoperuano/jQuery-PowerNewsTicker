07e042940ee412f39540cb8aff728a7c8cf3d0f5
Marco Ledesma <marco.j.ledesma@gmail.com>
2013-02-24 18:05:01 -0500

    Added new options and themes to the plugin

--- jquery.powernewsticker.js
+++ jquery.powernewsticker.js
@@ -13,24 +13,19 @@
 		var checkNumberOfRows = function(table) {
 			var numOfRows = $(table).children('tbody').children('tr').length;
 			var maxRows = parseInt($(table).attr('PowerNewsTicker_max_num_rows'));
+			var rowAnimSpeed = parseInt($(table).attr('PowerNewsTicker_row_anim_speed'));
 
 			if (numOfRows > maxRows)
 			{
 				//grab last row, convert it into a div, do cool animation!
-				var $lastRow = $(table).find('tr:not(.PowerNewsTicker_old_row)').last();
+				var $lastRow = $(table).find('tr:not([PowerNewsTicker_old])').last();
 				var $newRow = $("<tr>");
-				$lastRow.addClass('PowerNewsTicker_old_row').children('td').each(function() {
+				$lastRow.attr('PowerNewsTicker_old', 'true').children('td').each(function() {
 					var $td = $(this);
-					$td.html("<div>" + $td.html() + "</div>");
-					$td.css({
-						backgroundColor: "#E44848",
-						color: "#FFF"
-					});
+					$td.html("<div>" + $td.html() + "</div>").addClass('PowerNewsTicker_old_row');
 					
-					$td.children('div').slideUp({duration:"slow", queue:false });
-					$td.slideUp({ duration:"slow", queue:false} );
-					
-
+					$td.children('div').slideUp({duration:rowAnimSpeed + 300, queue:false });
+					$td.slideUp({duration:rowAnimSpeed + 300, queue:false});
 				});
 			}
 		};
@@ -38,6 +33,7 @@
 		//this will animate a table by shifting rows downward when a new entry is added
 		var insertNewRow = function(table, row) {
 			var newRow;
+			var rowAnimSpeed = parseInt($(table).attr('PowerNewsTicker_row_anim_speed'));
 			//for flashing the row when it comes into view
 			var flashNewRow = function(rowCell) {
 				//the class that has the color animation
@@ -77,8 +73,8 @@
 					$(newCell).html("<div style='display:none'>" + text + "</div>");
 
 					$(newCell).queue(function() {
-						$(newCell).slideDown({ duration: 400, queue: false });
-						$(newCell).children('div').slideDown({ duration: 400, queue: false  });
+						$(newCell).slideDown({ duration: rowAnimSpeed, queue: false });
+						$(newCell).children('div').slideDown({ duration: rowAnimSpeed, queue: false  });
 						$(newCell).dequeue();
 						flashNewRow(newCell);
 					});
@@ -92,7 +88,10 @@
 
 		if (options == undefined || typeof(options) == "object") {
 			options = $.extend({ 
-				maxNumRows: 10
+				maxNumRows: 10,
+				bootStrapStyle: true,
+				theme: "default", //possible values: sky, lava, dark, light,
+				rowAnimSpeed: 400
 			}, options);
 		} else {
 			switch (options) {
@@ -107,14 +106,61 @@
 
 		var styles = function() {
 			var $style = $("<style type='text/css'>");
+			var newRowBgColor = "";
+			var newRowTextColor = "";
+			var oldRowBgColor = "";
+			var oldRowTextColor = "";
+			switch (options.theme) {
+				case "default": 
+					newRowBgColor = "#006DAD";
+					newRowTextColor = "#FFF";
+					oldRowBgColor = "#E44848";
+					oldRowTextColor = "#FFF";
+				break;
+				case "sky":
+					newRowBgColor = "#007BB8";
+					newRowTextColor = "#FFF";
+					oldRowBgColor = "#3A4878";
+					oldRowTextColor = "#FFF";
+				break;
+				case "lava":
+					newRowBgColor = "#D68A28";
+					newRowTextColor = "#FFF";
+					oldRowBgColor = "#D64E28";
+					oldRowTextColor = "#FFF";
+				break;
+				case "dark":
+					newRowBgColor = "#222";
+					newRowTextColor = "#FFF";
+					oldRowBgColor = "#444";
+					oldRowTextColor = "#FFF";
+				break;
+				case "light":
+					newRowBgColor = "#E6E6E6";
+					newRowTextColor = "#333";
+					oldRowBgColor = "#CCC";
+					oldRowTextColor = "#333";
+				break;
+				default:
+					newRowBgColor = "#006DAD";
+					newRowTextColor = "#FFF";
+					oldRowBgColor = "#E44848";
+					oldRowTextColor = "#FFF";
+				break;
+			}
 			var properties = ".PowerNewsTicker_new_row {";
-			properties += "background-color:#006DAD; color:white;";
+			properties += "background-color:"+newRowBgColor+"; color:"+newRowTextColor+";";
+			properties += "}";
+			properties += ".PowerNewsTicker_old_row {";
+			properties += "background-color:"+oldRowBgColor+"; color:"+oldRowTextColor+";";
 			properties += "}";
 			properties += ".PowerNewsTicker_row {";
 			properties += "-webkit-transition:color, background-color .5s;";
 			properties += "-moz-transition:color, background-color .5s;";
 			properties += "-ms-transition:color, background-color .5s;";
 			properties += "}"
+
+			//grabbed this table style from twitter bootstrap
 			$style.html(properties);
 			$('body').append($style);
 		};
@@ -122,7 +168,6 @@
 
 		this.each(function() {
 			var $table = $(this);
-
 			//check if a row is inserted every couple of miliseconds
 			$table.children('tbody').children('tr').each(function() {
 				$(this).attr('PowerNewsTicker_monitored', 'true');
@@ -131,6 +176,10 @@
 
 			//add options attributes to table
 			$table.attr('PowerNewsTicker_max_num_rows', options.maxNumRows);
+			$table.attr('PowerNewsTicker_row_anim_speed', options.rowAnimSpeed);
+			//adding bootstrap styling
+			if (options.bootStrapStyle) $table.addClass('table').addClass('table-bordered');
+
 		});
 
 		styles();
--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 $(document).ready(function() {
 	var options = {
-		maxNumRows: 5
+		maxNumRows: 4,
+		theme: "daraka"
 	};
 	$('#records').PowerNewsTicker(options);
 
 	for (var i=0; i<=3;i++) {
-		$('body').delay(3500).queue(function() {
+		$('body').delay(3000).queue(function() {
 			var $row = $("<tr>");
 			var $td = $("<td>").html("Hello world");
 			var row = "<tr><td>Test</td><td>12/2/2009</td><td>Stuff</td></tr>";
